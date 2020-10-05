1f3247182521115655e93ed41cc244432fb0201b
Codrina Merigo <codrina.merigo@fmc-ag.com>
2020-10-05 21:17:12 +0200

    Add project files.

---
+++ SyncfusionChatUI/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SyncfusionChatUI.Services;
+using SyncfusionChatUI.Views;
+
+namespace SyncfusionChatUI
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
